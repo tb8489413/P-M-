a3487ed30a17f2edc686c2b75f54b12e217c2943
石头老师 <shitou1235813@163.com>
2019-10-20 15:11:13 +0800

    Add files via upload

---
+++ 飞艇.py
@@ -0,0 +1,43 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import mcpi.minecraftstuff as minecraftstuff
+import time
+mc = minecraft.Minecraft.create()
+mcdrawing = minecraftstuff.MinecraftDrawing(mc)
+pos = mc.player.getTilePos()
+
+for i in range(51):
+    mcdrawing.drawCircle(pos.x,pos.y+50,pos.z+i,10,42)
+for j in range(10):
+    mcdrawing.drawCircle(pos.x,pos.y+50,pos.z+50+j,10-j,42)
+for k in range(10):
+    mcdrawing.drawCircle(pos.x,pos.y+50,pos.z-k,10-k,42)
+
+wy_1 = []
+wy_1.append(minecraft.Vec3(pos.x,pos.y+50+10,pos.z+40))
+wy_1.append(minecraft.Vec3(pos.x,pos.y+50+10+5,pos.z+50))
+wy_1.append(minecraft.Vec3(pos.x,pos.y+50+10+9,pos.z+50+8))
+wy_1.append(minecraft.Vec3(pos.x,pos.y+50+2,pos.z+50+8))
+mcdrawing.drawFace(wy_1,True,42)
+
+wy_2 = []
+wy_2.append(minecraft.Vec3(pos.x,pos.y+50-10,pos.z+40))
+wy_2.append(minecraft.Vec3(pos.x,pos.y+50-10-5,pos.z+50))
+wy_2.append(minecraft.Vec3(pos.x,pos.y+50-10-9,pos.z+50+8))
+wy_2.append(minecraft.Vec3(pos.x,pos.y+50-2,pos.z+50+8))
+mcdrawing.drawFace(wy_2,True,42)
+
+wy_3 = []
+wy_3.append(minecraft.Vec3(pos.x+10,pos.y+50,pos.z+40))
+wy_3.append(minecraft.Vec3(pos.x+15,pos.y+50,pos.z+50))
+wy_3.append(minecraft.Vec3(pos.x+19,pos.y+50,pos.z+50+8))
+wy_3.append(minecraft.Vec3(pos.x+2,pos.y+50,pos.z+50+8))
+mcdrawing.drawFace(wy_3,True,42)
+
+wy_4 = []
+wy_4.append(minecraft.Vec3(pos.x-10,pos.y+50,pos.z+40))
+wy_4.append(minecraft.Vec3(pos.x-15,pos.y+50,pos.z+50))
+wy_4.append(minecraft.Vec3(pos.x-19,pos.y+50,pos.z+50+8))
+wy_4.append(minecraft.Vec3(pos.x-2,pos.y+50,pos.z+50+8))
+mcdrawing.drawFace(wy_4,True,42)
+
