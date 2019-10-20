06eedabed77f41872988a8d4a5651d15b261e35f
石头老师 <shitou1235813@163.com>
2019-10-20 17:50:36 +0800

    Update 飞艇.py
    添加部分注释

--- 飞艇.py
+++ 飞艇.py
@@ -6,13 +6,15 @@ mc = minecraft.Minecraft.create()
 mcdrawing = minecraftstuff.MinecraftDrawing(mc)
 pos = mc.player.getTilePos()
 
+#飞艇主体
 for i in range(51):
     mcdrawing.drawCircle(pos.x,pos.y+50,pos.z+i,10,42)
 for j in range(10):
     mcdrawing.drawCircle(pos.x,pos.y+50,pos.z+50+j,10-j,42)
 for k in range(10):
     mcdrawing.drawCircle(pos.x,pos.y+50,pos.z-k,10-k,42)
-
+    
+#飞艇尾翼
 wy_1 = []
 wy_1.append(minecraft.Vec3(pos.x,pos.y+50+10,pos.z+40))
 wy_1.append(minecraft.Vec3(pos.x,pos.y+50+10+5,pos.z+50))
