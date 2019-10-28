8b1c0f1f9ace0b74e105db593521053fb9d950d4
石头老师 <shitou1235813@163.com>
2019-10-28 19:06:52 +0800

    Add files via upload

---
+++ 植被创造1.py
@@ -0,0 +1,40 @@
+import mcpi.minecraft as minecraft
+import mcpi.block as block
+import time
+mc = minecraft.Minecraft.create()
+
+
+while True:
+    n1 = 18
+    n2 = 3
+    pos = mc.player.getTilePos()
+    pos.z = pos.z + 4
+    
+    #创建第一层树叶
+    mc.setBlocks(pos.x+2,pos.y+7,pos.z-1,pos.x+2,pos.y+7,pos.z+1,n1,n2)
+    mc.setBlocks(pos.x+1,pos.y+7,pos.z-2,pos.x-2,pos.y+7,pos.z+2,n1,n2)
+
+    #创建第二层树叶
+    mc.setBlocks(pos.x+2,pos.y+8,pos.z-1,pos.x+2,pos.y+8,pos.z+1,n1,n2)
+    mc.setBlocks(pos.x+1,pos.y+8,pos.z-2,pos.x-2,pos.y+8,pos.z+2,n1,n2)
+    mc.setBlock(pos.x-2,pos.y+8,pos.z-2,0)
+
+    #创建第三层树叶
+    mc.setBlocks(pos.x+1,pos.y+9,pos.z-1,pos.x-1,pos.y+9,pos.z+1,n1,n2)
+    mc.setBlock(pos.x-1,pos.y+9,pos.z-1,0)
+    mc.setBlock(pos.x-1,pos.y+9,pos.z+1,0)
+
+    #创建第四层树叶
+    mc.setBlocks(pos.x+1,pos.y+10,pos.z-1,pos.x-1,pos.y+10,pos.z+1,n1,n2)
+    mc.setBlock(pos.x-1,pos.y+10,pos.z-1,0)
+    mc.setBlock(pos.x-1,pos.y+10,pos.z+1,0)
+    mc.setBlock(pos.x+1,pos.y+10,pos.z-1,0)
+    mc.setBlock(pos.x+1,pos.y+10,pos.z+1,0)
+
+    #创建树干
+    mc.setBlocks(pos.x,pos.y,pos.z,pos.x,pos.y+9,pos.z,17,3)
+
+    time.sleep(3)
+    
+    
+    
